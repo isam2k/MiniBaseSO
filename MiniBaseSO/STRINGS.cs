f6b14a77afb0a7bfae15e7170ea14598238a033f
Samuel Aeberhard <samuel.aeberhard@gmail.com>
2024-12-12 10:38:31 +0100

    Updating and unifying formatting.

---
+++ MiniBaseSO/STRINGS.cs
@@ -0,0 +1,53 @@
+namespace MiniBaseSO
+{
+    internal class STRINGS
+    {
+        public class WORLDS
+        {
+            public class MINIBASE
+            {
+                public static LocString NAME = "MiniBase";
+                public static LocString DESCRIPTION = "An encapsulated location with just enough to get by.\n\n<smallcaps>Customize this location by clicking MiniBase Options in the Mods menu.</smallcaps>\n\n";
+            }
+
+            public class MARSHYMINIBASE
+            {
+                public static LocString NAME = "MiniBase Marshy Moonlet";
+                public static LocString DESCRIPTION = "A marshy world, with a curious cybernetic tree";
+            }
+
+            public class OILYMINIBASE
+            {
+                public static LocString NAME = "MiniBase Oily Moonlet";
+                public static LocString DESCRIPTION = "A sulfurous world, with plenty of oil, rare minerals and metals";
+            }
+
+            public class NIOBIUMMINIBASE
+            {
+                public static LocString NAME = "MiniBase Superconductive Moonlet";
+                public static LocString DESCRIPTION = "An inhospitable world full of magma and precious metals";
+            }
+        }
+
+        public class UI
+        {
+            public class SPACEDESTINATIONS
+            {
+                public class HARVESTABLE_POI
+                {
+                    public class NIOBIUMASTEROIDFIELD
+                    {
+                        public static LocString NAME = "Niobium asteroid field";
+                        public static LocString DESC = "An asteroid field containing traces of niobium";
+                    }
+
+                    public class RESINASTEROIDFIELD
+                    {
+                        public static LocString NAME = "Resin asteroid field";
+                        public static LocString DESC = "An asteroid field with plenty of liquid resin";
+                    }
+                }
+            }
+        }
+    }
+}
